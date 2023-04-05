d9aaffd46b91b4ded462236e83ed2e0244da7dae
valike23 <ludike23@gmail.com>
2023-04-05 16:54:14 +0100

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { accountRouter } from './router/accounts';
 import {json} from "body-parser";
+import {cors} from "cors";
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 app.use(json());
+app.use(cors());
 app.use('/accounts', accountRouter);
 
 app.listen(port, () => {
