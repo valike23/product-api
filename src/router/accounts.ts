525169cfc9126c6664c3b252f0bc48995a12c4a3
valike23 <ludike23@gmail.com>
2023-04-06 14:21:31 +0100

    changed the url

--- src/router/accounts.ts
+++ src/router/accounts.ts
@@ -5,5 +5,5 @@ export const  accountRouter = Router();
 
 accountRouter.put('/login', loginCtrl);
 accountRouter.post('/register', registerCtrl);
-accountRouter.put('/admin-login', adminLoginCtrl);
-accountRouter.post('/admin-register', adminRegisterCtrl);
+accountRouter.put('/admin/login', adminLoginCtrl);
+accountRouter.post('/admin/register', adminRegisterCtrl);
