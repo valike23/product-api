facd0229a49fdf82100cb363762d0f346af4689e
valike23 <ludike23@gmail.com>
2023-04-18 08:53:19 +0100

    fix the media file

--- src/controllers/productCtrl.ts
+++ src/controllers/productCtrl.ts
@@ -2,19 +2,20 @@ import { Request, Response } from "express";
 import { Cloudinary } from "../helphers/cloudinary";
 import { Imedia, Isize, Product } from "../helphers/product";
 import { Category } from "../helphers/category";
+const cloudinary = require('cloudinary').v2;
 
 export const addProductCtrl = async (req: any, res: Response) => {
     const body = req.body;
     body.author = req["user"].id;
     const product = new Product(body);
-    let prod : any= await product.save();
+    let prod: any = await product.save();
     let category = new Category();
     let id = prod.resp[0];
-    body.categories.forEach((_e: any,_i: any)=>{
+    body.categories.forEach((_e: any, _i: any) => {
         body.categories[_i].product_id = id;
     });
     const cate = await category.save(body.categories);
-    if (prod.status == 'success') return res.json({prod, cate});
+    if (prod.status == 'success') return res.json({ prod, cate });
     res.status(503).json(prod);
 }
 
@@ -55,9 +56,19 @@ export const uploadCtrl = async (req: any, res: Response) => {
 
         console.log('files', req.files.media.path);
         const cloud = new Cloudinary();
-        let image = await cloud.uploadContent(req.files.media.path);
+
+        // Configuration 
+        cloudinary.config({
+            cloud_name: "tjconnect",
+            api_key: "556459332373436",
+            api_secret: "-vfzmuQdlkLrB1rdqR5hTAf5wJg"
+        });
+
+        const red = await cloudinary.uploader.upload(req.files.media.path);
+
+      //  let image = await cloud.uploadContent(req.files.media.path);
         let body: Imedia = {
-            url: image.secure_url,
+            url: red.secure_url,
             type,
             height,
             width
